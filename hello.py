fc6159b89cfc1a6a6076e1a9f3c67c45cee12629
Indraneel Purohit <i.m.indraneel@gmail.com>
2012-10-13 19:31:07 -0400

    added json

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
-import simplejson
+import simplejson as json
+import requests
 app = Flask(__name__)
+api_urls = {'nextbus': 'http://nextbus.nodejitsu.com/'}
 
-@app.route('/')
-def hello_world():
-    return render_template('home.html', variable="asdf")
+@app.route('/<stop>')
+def hello_world(stop):
+    r = requests.get('http://nextbus.nodejitsu.com/route/{}'.format(stop))
+    jsonData = json.loads(r.text)
+    print jsonData
+    return render_template('home.html', data=jsonData)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
