cd9297d03fbad1a5f6900bbaa1ca89a05ef160d2
Indraneel Purohit <i.m.indraneel@gmail.com>
2012-10-13 18:21:08 -0400

    render template

---
+++ hello.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+import simplejson
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('home.html', variable="asdf")
+
+if __name__ == '__main__':
+    app.run(debug=True)
