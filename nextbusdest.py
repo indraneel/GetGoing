3129f38f370c48a14d11ad27f84417dbfa1be025
lavifb <lavifb@gmail.com>
2012-10-19 00:51:16 -0400

    fixed bug in search form

--- nextbusdest.py
+++ nextbusdest.py
@@ -103,9 +103,9 @@ def nextBus():
         startLoc = request.args.get('start', '')
         # print(startLoc)
         destLoc = request.args.get('destination', '')
-        # print(destLoc)        
+        print(destLoc)     
         lat = request.args.get('latitude', '')
-        # print(lat)
+        print(lat)
         lon = request.args.get('longitude', '')
         # print(lon)
 
